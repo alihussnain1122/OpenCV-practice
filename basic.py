c3a6179271b3cd2fe8549138e833009dbdec1c06
Ali Hussnain <2023cs613@student.uet.edu.pk>
2025-10-02 23:43:01 +0500

    dilating the image edges

--- basic.py
+++ basic.py
@@ -22,4 +22,9 @@ cv.imshow('Blur', blur)
 #Edge Cascade
 canny= cv.Canny(resize(image), 125,175)
 cv.imshow('Canny Edges', canny)
+#cv.waitKey(0)
+
+#Dilating the image
+dilated= cv.dilate(canny, (7,7), iterations=3)
+cv.imshow('Dilated', dilated)
 cv.waitKey(0)
